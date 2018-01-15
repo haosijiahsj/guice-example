0da3cddf8867ef40cc588944971f318bc0af4e11
胡胜钧 <1017547773@qq.com>
2018-01-15 21:33:11 +0800

    添加事务

--- src/main/java/com/zzz/service/impl/UserServiceImpl.java
+++ src/main/java/com/zzz/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package com.zzz.service.impl;
 
 import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import com.zzz.dao.UserMapper;
 import com.zzz.model.UserVo;
 import com.zzz.service.UserService;
+import org.mybatis.guice.transactional.Transactional;
 
 import java.util.List;
 
@@ -12,11 +14,12 @@ import java.util.List;
  * @author 胡胜钧
  * @date 1/14 0014.
  */
+@Singleton
+@Transactional
 public class UserServiceImpl implements UserService {
 
     @Inject
     private UserMapper userMapper;
-
 
     @Override
     public UserVo getByUsername(String username) {
